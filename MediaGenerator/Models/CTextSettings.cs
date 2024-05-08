3e1832c2fa0720e28485e9f76059d5b6113b7635
James W <jameswasiew@gmail.com>
2024-05-08 13:31:13 -0400

    Created CTextSettings

---
+++ MediaGenerator/Models/CTextSettings.cs
@@ -0,0 +1,22 @@
+﻿namespace MediaGenerator.Models
+{
+    public class CTextSettings
+    {
+        string id { get; set; }
+        public string textMessage { get; set; }
+        public int fontSize { get; set; }
+        public string fontFamily { get; set; }
+
+        public List<eFontWeight> fontWeights { get; set; }
+
+        public enum eFontWeight {
+            Regular,
+            Bold,
+            Italic,
+            Underlined
+        }
+
+        // need to store info like rotation, gradient over time
+        // try setting up something in javascript and see how the data looks
+    }
+}
